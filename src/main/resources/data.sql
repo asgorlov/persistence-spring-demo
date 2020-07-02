40fa68d925f818cc487a09e2ff75be5dc3f961b9
asgorlov <asgorlov@outlook.com>
2020-07-02 15:11:47 +0300

    Creating a Test Database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO officers(rank, first_name, last_name) VALUES('Captain', 'James', 'Bond');
+INSERT INTO officers(rank, first_name, last_name) VALUES('Captain', 'Lukas', 'Tompson');
+INSERT INTO officers(rank, first_name, last_name) VALUES('Captain', 'John', 'Lil');
+INSERT INTO officers(rank, first_name, last_name) VALUES('Captain', 'Tom', 'Sort');
+INSERT INTO officers(rank, first_name, last_name) VALUES('Captain', 'Vova', 'Kren');
